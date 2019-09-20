84822b82a4c16dea11944efb2a92234a7d656c0a
Daniel <cermak222@gmail.com>
2019-09-20 15:36:59 -0500

    Revert "add wish"
    This reverts commit 3f14b2c3ddf76a5dd4c2ca7ff2176375ccbf83e1.

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.wish_index, name='wish_index'),
-    path('add/', views.WishCreate.as_view(), name='wish_create'),
-    path('delete/<int:wish_id>', views.WishDelete.as_view(), name='wish_delete'),
+    # path('add/', views.Wish.as_index(), name='add'),
+    # path('delete/<int:item_id>', views.delete, name='delete'),
 ]
-
